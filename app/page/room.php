b1d4c8f9625ace58714e9aab56627aa06fd40533
Emily <emilycxm-pm25@student.tarc.edu.my>
2026-07-27 12:29:56 +0800

    Update

--- app/page/room.php
+++ app/page/room.php
@@ -10,14 +10,15 @@ if($user_id ==''){
 
     redirect('home.php');
 }else{
-	$stm=$_db->prepare("SELECT res.reservation_id,res.user_id res.start_time,
-	res.end_time, res.booking_date, ts.start_time, ts.end_time, rm.name AS room_name,
+	$stm=$_db->prepare("SELECT res.reservation_id,res.user_id ,res.start_time,
+	res.end_time, res.booking_date, ts.start_time, ts.end_time, u.user_id, u.name AS user_name,rm.name AS room_name,
 	rm.description, rm.status AS room_status,
 	rm.capacity 
 	FROM reservation res 
-	JOIN reservation_room r ON res.reservation_id == r.reservation_id
+	JOIN reservation_room r ON res.reservation_id = r.reservation_id
 	JOIN room rm ON res.room_id = rm.room_id 
 	JOIN time_slot ts ON res.reservation_id = ts.slot_id 
+	LEFT JOIN user u ON res.user_id = u.user_id
 	WHERE r.user_id=?
 	ORDER BY res.booking_date DESC 
 ");
@@ -28,7 +29,7 @@ if($user_id ==''){
 $_title = "Room History";
 include '../_head.php';
 ?>
-<?php if($shows == ''): 
+<?php if(empty($shows)): 
 	temp('info','No record yet');
 	?>
 	<?php else:?>
@@ -47,19 +48,21 @@ include '../_head.php';
 			</tr>
 		<thead>
 			<tbody>
-				<td><?=htmlspecialchars($show['reservation_id'])?></td>
-				<td><?= htmlspecialchars($show['user_id']) ?></td>
-				<td><?= $show['room_name'] ?></td>
-				<td> <?= $show['room_status'] === 'confirm' ? 'green' : 'red' ?>;">
-				<?= $show['room_status']?></td>
-				<td> <?= $show['description']?></td>
-				<td><?= $show['capacity']?></td>
-				<td><?= $show['start_time'] ?> - <?=$show['end_time']?></td>
-				<td><?= $show['booking_date']?></td>
-				
+				<tr>
+					<td><?=htmlspecialchars($show['reservation_id'])?></td>
+					<td><?= htmlspecialchars($show['user_id']) ?></td>
+					<td><?= htmlspecialchars($show['room_name']) ?></td>
+					<td> <?=$show['room_status'] === 'confirm' ? 'green' : 'red' ?>;">
+					<?= htmlspecialchars($show['room_status'])?></td>
+					<td> <?=htmlspecialchars($show['description'])?></td>
+					<td><?= htmlspecialchars($show['capacity'])?></td>
+					<td><?= htmlspecialchars($show['start_time'] )?> - <?=htmlspecialchars($show['end_time'])?></td>
+					<td><?= htmlspecialchars($show['booking_date'])?></td>
+				</tr>
+				<?php endforeach ?>
 			</tbody>
 	
 </table>
-<?php endforeach ?>
+
 <?php endif ?>
 <?php include '../_foot.php'; ?>
