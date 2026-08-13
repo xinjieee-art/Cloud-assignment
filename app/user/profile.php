1b6209e219f73ba9568d87bce9edfe36bbb3fd8a
Emily <emilycxm-pm25@student.tarc.edu.my>
2026-08-13 15:20:43 +0800

    Profile user side is work

--- app/user/profile.php
+++ app/user/profile.php
@@ -21,8 +21,7 @@
     if (is_post()) {
         $email = req('email');
         $name  = req('name');
-        $phone = req('phone');
-        $photo = $_SESSION['image_url'];
+        $photo = $_SESSION['image_url'] ?? 'images/profile/default.png';
         $f = get_file('photo');
 
         if ($email == '') {
@@ -52,19 +51,13 @@
         else if (strlen($name) > 100) {
             $_err['name'] = 'Maximum 100 characters';
         }
-        if ($phone == '') {
-            $_err['phone'] = 'Required';
-        }
-        else if (!preg_match ('/^\d{10,11}$/', $phone)) {
-            $_err['phone'] = 'Invalid phone number';
-        }
 
         if ($f) {
             if (!str_starts_with($f->type, 'image/')) {
-                $_err['photo'] = 'Must be image';
+                $_err['profile'] = 'Must be image';
             }
             else if ($f->size > 1 * 720 * 720) {
-                $_err['photo'] = 'Maximum 1MB';
+                $_err['profile'] = 'Maximum 1MB';
             }
         }
 
@@ -72,11 +65,17 @@
         if (!$_err) {
 
             if ($f) {
-                unlink("../images/$photo");
-                $photo = save_photo($f, '../images/profile','/images/profile');
+       
+        if (!empty($_user->profile) && $_user->profile !== 'images/profile/default.png') {
+            $old_file = '../' . $_user->profile;
+            if (file_exists($old_file) && is_file($old_file)) {
+                unlink($old_file);
             }
+        }
+             $photo = save_photo($f, '../images/profile', 200);
+    }
             $stm = $_db->prepare('
-                UPDATE member
+                UPDATE user
                 SET email = ?, name = ?, profile = ?
                 WHERE user_id = ?
             ');
@@ -91,39 +90,7 @@
         }
     }
 
-   // $stm = $_db->prepare('SELECT * FROM invoice WHERE user_id = ?');
-   // $stm->execute([$_user->user_id]);
-    //$orders = $stm->fetchAll();
-
- //   if (is_post() && isset($_POST['cancel_order_id'])) {
-  //      $cancel_id = post('cancel_order_id');
-
-        
- //       $stmt = $_db->prepare("SELECT product_id, quantity FROM order_details WHERE order_id = ?");
- //       $stmt->execute([$cancel_id]);
- //        $items = $stmt->fetchAll();
-
- //       foreach ($items as $item) {
- //           $upd = $_db->prepare("UPDATE product SET stock_quantity = stock_quantity + ? WHERE product_id = ?");
- //           $upd->execute([$item->quantity, $item->product_id]);
- //       }
-
- //       $delDetails = $_db->prepare("DELETE FROM order_details WHERE order_id = ?");
-  //      $delDetails->execute([$cancel_id]);
-
-        
-  //      $delReceipt = $_db->prepare("DELETE FROM receipt WHERE order_id = ?");
- //       $delReceipt->execute([$cancel_id]);
-
-       
- //       $delInvoice = $_db->prepare("DELETE FROM invoice WHERE order_id = ?");
- //       $delInvoice->execute([$cancel_id]);
-
- //       temp('info', 'Order cancelled and records cleared.');
- //       redirect($_SERVER['PHP_SELF']);
- //       exit();
-  //  }
-
+  
 ?>
 
 <form method="post" class="profileForm" enctype="multipart/form-data">
@@ -152,35 +119,7 @@
     </section>
 </form>
 
-<div class = "profileOrderHistory">
-    <h2>Order History</h2>
-    <?php if($orders): ?>
-        <table>
-            <?php foreach($orders as $o): ?>       
-            <tr class = profileOrderRowTop>
-                <td class = profileOrderDetails><b>Order #<?= $o->order_id." ". $o->order_date ?></b></td>
-                <td></td>
-                <td class = "profileOrderViewDetailBtn">
-                    <button data-post="order_detail.php?order_id=<?= $o->order_id ?>">View Details</button>
-                    <?php if ($o->status == 'Preparing'): ?>
-                        <form method="post" onsubmit="return confirm('Are you sure you want to cancel this order? This will restore stock and delete the record.')">
-                            <input type="hidden" name="cancel_order_id" value="<?= $o->order_id ?>">
-                            <button type="submit" class="cancel-btn" style="color: red; font-size: 20px; text-decoration: underline;">Cancel Order</button>
-                        </form>
-                    <?php endif; ?>
-                </td>
-            </tr>
-            <tr class = profileOrderRowMiddle>
-                <td class = "profileOrderAddress"><span>Shipping Address:</span><br><?= $o->street_address . ",<br>" . $o->city . ', ' . $o->state . ' ' . $o->post_code;?></td>
-                <td class = profileOrderPrice>RM <?= number_format($o->total_amount,2) ?></td>
-                <td class = "profileOrderProductQuantiy">Status: <?= $o->status ?></td>
-            </tr>
-            <tr class = profileOrderRowBottom></tr>
-            <?php endforeach ?>
-        </table>
-    <?php else: ?>
-        <p class = "profileOrderEmpty">You have no previous orders!</p>
-    <?php endif ?>
+
 </div>
 
 <?php
